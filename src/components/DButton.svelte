610eeeccf78951763324407bef482050705c820e
Steve Pabatao <spabatao@lull.com>
2021-04-07 08:36:31 +0800

    Added the sending of etherium

--- src/components/DButton.svelte
+++ src/components/DButton.svelte
@@ -8,30 +8,53 @@ let netv = "";
 
 
 async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_accounts' });
-  const network_version = await ethereum.request({ method: 'net_version' });
-  
+   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+   const network_version = await ethereum.request({ method: 'net_version' });
 
    if(network_version == 3) {
         netv = "Ropstein Test Netwoek";
    }
 
    account = accounts[0];
-/*
-   Ethereum Main Network (Mainnet)
-0x3	3	Ropsten Test Network
-0x4	4	Rinkeby Test Network
-0x5	5	Goerli Test Network
-0x2a	42	Kovan Test Network
-*/
-  //getValue(account);
-  alert(n[netv]);
+}
+
+async function sendEther() {
+    alert(getAccount);
+    ethereum
+        .request({
+        method: 'eth_sendTransaction',
+        params: [
+            {
+            from: account,
+            to: '0x0F49e71bC4FcA91E020F60f094c02B2Cf7b42561',
+            value: '0x29a2241af62c0000',
+            gasPrice: '0x09184e72a000',
+            gas: '0x2710',
+            },
+        ],
+        })
+        .then((txHash) => console.log(txHash))
+        .catch((error) => console.error);
 }
 
 
 </script>
-<button class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" on:click={getAccount}>
-    Enable Ethereum
-</button>
-<h3>Account: <span>{ account }</span></h3>
-<h3>Net Version: <span>{ netv }</span></h3>
+
+
+
+
+    <div class="max-w-lg w-full rounded-lg shadow-lg p-4">
+        <h3 class="font-semibold text-lg text-gray-700 tracking-wide">Create new account</h3>
+            <ul>
+                <li><span class="text-sm">Account: <span>{ account }</span></span></li>
+                <li><span class="text-sm">Net Version: <span>{ netv }</span></span></li>
+            </ul>
+        <div>
+            <button class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" on:click={getAccount}>
+                Enable Ethereum
+            </button>
+            <button class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" on:click={sendEther}>
+                Send Ethereum
+            </button>
+        </div>
+    </div>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,10 +15,26 @@
     }
   </script>
 
-<div class="flex flex-wrap py-2">
-    <div class="w-full px-4">
-      <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-200 rounded">
-        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+<div class="flex 
+            flex-wrap 
+            py-2">
+    <div class="w-full 
+                px-4">
+      <nav class="relative 
+                  flex 
+                  flex-wrap 
+                  items-center 
+                  justify-between 
+                  px-2 py-3 
+                  bg-gray-200 
+                  rounded">
+        <div class="container 
+                    px-4 
+                    mx-auto 
+                    flex 
+                    flex-wrap 
+                    items-center 
+                    justify-between">
           <div class="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
             <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-500" href="/">
               D Cart
@@ -42,12 +58,15 @@
                   </a>
               </li>
               <li class="nav-item">
-                <DButton/> 
+                
               </li>  
             </ul>
           </div>
         </div>
       </nav>
     </div>
+  </div>
+  <div class="flex flex-wrap right py-2  right-0">
+    <DButton/> 
   </div>
 
